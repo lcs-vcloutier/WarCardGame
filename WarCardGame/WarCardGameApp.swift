5a3e02c95ef3aed1705780e02efb9c95a8dc091d
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-04 10:31:03 -0500

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Cloutier, Vincent on 2020-11-04.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
